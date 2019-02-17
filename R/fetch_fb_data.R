c7e10e7aec93f34d047b754934c515918fbed34c
lynuhs <linus@lynuhs.com>
2019-02-17 01:08:53 +0100

    Update fetch_fb_data.R

--- R/fetch_fb_data.R
+++ R/fetch_fb_data.R
@@ -5,10 +5,14 @@
 #' @param scope The scope to select data from.
 #' @param parameters The parameters to download. Breakdowns, dimensions and metrics.
 #' @param api_version The version of Facebook API.
+#' @import httr
 #' @export
 #' @examples
 #' fetch_fb_data(object=c("me","accounts","campaigns",fb_userId()), scope=c("adaccounts","insights"), parameters="", api_version="3.2")
 fetch_fb_data <- function(object, scope = "", parameters = "", api_version = "3.2"){
+  if(!fb_check_existing_token()){
+    stop("Error: No authenticated token found!")
+  }
   data <- GET(url = paste0(
     paste0("https://graph.facebook.com/v",api_version,"/"),
     object,"/",
